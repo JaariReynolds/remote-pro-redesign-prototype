fa46a8a0c604b8940f43bcf99afb7dc2b09d4e22
Jaari <jaari.rey@gmail.com>
2023-12-15 19:30:16 +1000

    step state controlled by navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
+import { useState } from "react"
 import "./App.scss"
 import BuilderOptions from "./components/builderOptions/BuilderOptions"
 import CurrentConfiguration from "./components/currentConfiguration/CurrentConfiguration"
 import Navigation from "./components/navigation/Navigation"
 import Step from "./components/step/Step"
 
+export const STEP_NAMES = ["1", "2", "3", "4", "5"]
+
 function App() {
+  const [currentStep, setCurrentStep] = useState<number>(0)
+
   return (
     <div className="app-container">
-      <Step />
+      <Step currentStep={currentStep} />
       <CurrentConfiguration />
       <BuilderOptions />
-      <Navigation />
+      <Navigation currentStep={currentStep} setCurrentStep={setCurrentStep} />
     </div>
   )
 }
