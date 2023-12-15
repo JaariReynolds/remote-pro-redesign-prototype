ea522e0a71970943afa8821ebcc5ecc31890dcb2
Jaari <jaari.rey@gmail.com>
2023-12-15 19:27:17 +1000

    base navigation styling + functionality

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,7 +1,38 @@
+import { Dispatch, SetStateAction } from "react"
+import { STEP_NAMES } from "../../App"
 import "./navigation.scss"
 
-const Navigation = () => {
-  return <div className="navigation-container">Navigation component</div>
+interface NavigationProps {
+  currentStep: number
+  setCurrentStep: Dispatch<SetStateAction<number>>
+}
+
+const Navigation = ({ currentStep, setCurrentStep }: NavigationProps) => {
+  const handlePreviousStep = () => {
+    if (currentStep === 0) return
+
+    setCurrentStep((prevStep) => prevStep - 1)
+  }
+
+  const handleNextStep = () => {
+    if (currentStep === STEP_NAMES.length - 1) return
+
+    setCurrentStep((prevStep) => prevStep + 1)
+  }
+  return (
+    <div className="navigation-container">
+      <button
+        className="nav-button"
+        onClick={handlePreviousStep}
+        disabled={currentStep === 0}
+      >
+        Back
+      </button>
+      <button className="nav-button" onClick={handleNextStep}>
+        {currentStep < STEP_NAMES.length - 1 ? "Next" : "Search"}
+      </button>
+    </div>
+  )
 }
 
 export default Navigation
