b10831919c6b6231d3d3a515ab474a107d13a83d
Jaari <jaari.rey@gmail.com>
2023-12-15 12:46:47 +1000

    base components added to Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css"
+import "./App.scss"
+import BuilderOptions from "./components/builderOptions/BuilderOptions"
+import CurrentConfiguration from "./components/currentConfiguration/CurrentConfiguration"
+import Navigation from "./components/navigation/Navigation"
+import Step from "./components/step/Step"
 
 function App() {
-  return <>Hello, world</>
+  return (
+    <div className="app-container">
+      <Step />
+      <CurrentConfiguration />
+      <BuilderOptions />
+      <Navigation />
+    </div>
+  )
 }
 
 export default App
