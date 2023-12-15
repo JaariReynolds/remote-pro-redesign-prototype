74abb3e9939cf35bcdad5abe56369761307671d9
Jaari <jaari.rey@gmail.com>
2023-12-15 19:27:02 +1000

    base step styling + functionality

--- src/components/step/Step.tsx
+++ src/components/step/Step.tsx
@@ -1,7 +1,27 @@
+import { STEP_NAMES } from "../../App"
 import "./step.scss"
 
-const Step = () => {
-  return <div className="step-container">Step component</div>
+interface StepProps {
+  currentStep: number
+}
+
+const Step = ({ currentStep }: StepProps) => {
+  const getStepStyling = (stepIndex: number): React.CSSProperties => {
+    if (currentStep === stepIndex) {
+      return { backgroundColor: "yellow" }
+    }
+    return { backgroundColor: "blue" }
+  }
+
+  return (
+    <div className="step-container">
+      {STEP_NAMES.map((step, index) => (
+        <div className="step" key={index} style={getStepStyling(index)}>
+          {step}
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Step
