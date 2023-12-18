b8dc3fcd4bcd634cdf182edde2111bd1122c93a4
Jaari <jaari.rey@gmail.com>
2023-12-18 23:23:20 +1000

    currentConfig shows currently selected builderOptions

--- src/components/currentConfiguration/CurrentConfiguration.tsx
+++ src/components/currentConfiguration/CurrentConfiguration.tsx
@@ -1,9 +1,43 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  BuilderOption,
+  builderOptionsArray,
+} from "../../builderOptionInformation"
 import "./current-configuration.scss"
+import { useEffect, useState } from "react"
 
-const CurrentConfiguration = () => {
+interface CurrentConfigurationProps {
+  currentSelectionIndexes: number[]
+}
+
+const CurrentConfiguration = ({
+  currentSelectionIndexes,
+}: CurrentConfigurationProps) => {
+  const [currentConfiguration, setCurrentConfiguration] = useState<
+    BuilderOption[]
+  >([])
+
+  useEffect(() => {
+    // get the builderOptions for each step based on the indexes
+    const newBuilderOptions = currentSelectionIndexes
+      .map((selectedIndex, arrayIndex) => {
+        if (selectedIndex !== -1)
+          // -1 predefined as an "unselection" i.e. no builder option selected
+          return builderOptionsArray[arrayIndex][selectedIndex]
+      })
+      .filter((item) => item !== undefined) as BuilderOption[]
+
+    setCurrentConfiguration(newBuilderOptions)
+  }, [currentSelectionIndexes])
+
   return (
     <div className="current-configuration-container">
-      CurrentConfiguration component
+      {currentConfiguration.map((builderOption, index) => (
+        <div key={index}>
+          <FontAwesomeIcon icon={builderOption.icon} />
+          <div>{builderOption.title}</div>
+        </div>
+      ))}
     </div>
   )
 }
